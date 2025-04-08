6a971997531931d8c1811c7361b073de6cb62987
stephentyers1975 <steve.tyers@gmail.com>
2025-04-08 11:04:38 +0100

    updated azurerm version

--- src/terraform/versions.tf
+++ src/terraform/versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.75.0"
+      # version = "~> 3.75.0"
+      version = "~> 4.26.0"
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
